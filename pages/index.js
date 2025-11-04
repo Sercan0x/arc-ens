23d3c51de764a2a0ff598486eb24b35b2c533292
Sercan0x <ahmet.kurnaz19239999@gmail.com>
2025-11-04 10:09:57 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -59,7 +59,7 @@ export default function Home() {
         fontFamily: "sans-serif",
         textAlign: "center",
         marginTop: 50,
-        backgroundImage: "url('https://i.imgur.com/Fqy6Ifn.png')",
+        backgroundImage: "url('https://i.imgur.com/kv9VdOo.png')",
         backgroundSize: "cover",
         backgroundPosition: "center",
         height: "100vh",
